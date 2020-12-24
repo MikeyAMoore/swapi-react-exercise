ea6303a309c0e89cb32c196557ae2937e0237660
Michael Andrew Moore <74065788+MikeyAMoore@users.noreply.github.com>
2020-12-23 20:05:18 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles.css";
+import SwapiData from "./components/swapicharacter";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>The force will be with you....always</h1>
+      <SwapiData />
+    </div>
+  );
+}
